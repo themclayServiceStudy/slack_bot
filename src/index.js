5d5c84ee1b23071c4fb693549d7555515c72c6ec
hanseuk-Choi <hschoi5542@gmail.com>
2023-10-21 15:37:52 +0900

    feat : env 파일 적용

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-const { App } = require('@slack/bolt');
+/**
+ *  동작 방식 -> slack bolt를 이용해서 API 호출
+ */
+
+const { App } = require('@slack/bolt'); // https://slack.dev/bolt-js/tutorial/getting-starteds
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// 사용자는 botToken과 secret만을 정의해서 구동할 수 있게 처리 -> slack bolt의 사용 객체로 정의하기 위함
 const botToken = 'xoxb-5846047594821-5977040764706-xp6Js4LAHlRrWCwRBPS4ImcA';
 const secret = '23c5e578402d768f0f067aa19f3e573a';
 
+// App 객체 정의
 const app = new App({
     token: botToken,
     signingSecret: secret,
@@ -22,13 +28,13 @@ const app = new App({
 //     }
 // });
 
-
-app.message(async ({ message, say }) => {
+// app.message(메세지 패턴, 콜백)
+app.message(':wave:', async ({ message, say }) => { // message 메소드를 통해 메세지를 들음
     // 콘솔에 메시지 출력
     console.log(`Received message: ${message.text} from channel: ${message.channel}`);
   
     // 메시지를 발송 채널로 다시 전송
-    await say(`Received your message: ${message.text}`);
+    await say(`Hello <@${message.user}> Received your message: ${message.text}`);
 });
 
 (async () => {
@@ -42,7 +48,7 @@ app.message(async ({ message, say }) => {
 
 
 /**
- *  express 설정
+ *  express 설정 -> 메세지 전송 api 
  */
 const server = express();
 server.use(bodyParser.json());
